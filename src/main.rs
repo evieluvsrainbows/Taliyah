541f22be46e272365e7ac288be7b6bef03a70cb4
Evelyn Marie <evelynelizabethmarie@gmail.com>
2022-10-10 08:38:28 -0600

    chore: minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-//! Ellie for Discord
-//!
-//! Ellie is a bot for the Discord chat platform focused on giving users
-//! a powerful set of features, while remaining quick to respond.
-
+use crate::serenity::GatewayIntents;
 use poise::serenity_prelude as serenity;
 use poise::{builtins::register_application_commands_buttons, Framework, FrameworkOptions, PrefixFrameworkOptions};
-
-use tracing::{info, instrument, Level};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, FmtSubscriber};
 use utils::read_config;
-
-use crate::serenity::GatewayIntents;
 
 mod config;
 mod data;
@@ -19,55 +9,30 @@ mod utils;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
 struct Data {}
 
 #[poise::command(prefix_command, owners_only)]
-async fn register_commands(ctx: Context<'_>) -> Result<(), Error> {
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
     register_application_commands_buttons(ctx).await?;
     Ok(())
 }
 
-#[tokio::main]
-#[instrument]
+#[tokio::main(worker_threads = 16)]
 async fn main() -> Result<(), Error> {
     let configuration = read_config("config.toml");
-    if configuration.bot.logging.enabled {
-        LogTracer::init()?;
-
-        let level = match configuration.bot.logging.level.as_str() {
-            "error" => Level::ERROR,
-            "warn" => Level::WARN,
-            "info" => Level::INFO,
-            "debug" => Level::DEBUG,
-            "trace" => Level::TRACE,
-            _ => Level::TRACE
-        };
-
-        let subscriber = FmtSubscriber::builder()
-            .with_target(false)
-            .with_max_level(level)
-            .with_env_filter(EnvFilter::from_default_env())
-            .finish();
-
-        tracing::subscriber::set_global_default(subscriber)?;
-
-        info!("Tracing initialized with logging level set to {}.", level);
-    }
-
     let token = configuration.bot.discord.token;
-    let prefix = configuration.bot.general.prefix;
-
     let framework = Framework::builder()
         .options(FrameworkOptions {
-            commands: vec![register_commands()],
+            commands: vec![register()],
             prefix_options: PrefixFrameworkOptions {
-                prefix: Some(prefix),
+                prefix: Some(configuration.bot.general.prefix),
                 ..Default::default()
             },
             ..Default::default()
         })
         .token(token)
-        .intents(GatewayIntents::non_privileged())
+        .intents(GatewayIntents::all())
         .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
 
     framework.run().await.unwrap();
