7ac042b7c580f4b0fab60a7cc5f72dfdaae7e6f6
Evelyn Harthbrooke <evelynharthbrooke@gmail.com>
2024-07-19 04:58:50 -0600

    chore(commands): rearrange the command source code.

--- src/commands/utilities/mod.rs
+++ src/commands/utilities/mod.rs
@@ -2,6 +2,14 @@ use poise::command;
 
 use crate::{Context, Error};
 
+/// Shows the help menu.
+#[command(slash_command, track_edits)]
+pub async fn help(ctx: Context<'_>, #[description = "Specific command to show help about"] command: Option<String>) -> Result<(), Error> {
+    let config = poise::builtins::HelpConfiguration { ..Default::default() };
+    poise::builtins::help(ctx, command.as_deref(), config).await?;
+    Ok(())
+}
+
 /// Says hello to the user who initializes the command.
 #[command(slash_command)]
 pub async fn hello(ctx: Context<'_>) -> Result<(), Error> {
@@ -13,13 +21,5 @@ pub async fn hello(ctx: Context<'_>) -> Result<(), Error> {
 #[command(slash_command)]
 pub async fn source(ctx: Context<'_>) -> Result<(), Error> {
     ctx.reply("GitHub repository: <https://github.com/evelynharthbrooke/Taliyah>").await?;
-    Ok(())
-}
-
-/// Shows the help menu.
-#[command(slash_command, track_edits)]
-pub async fn help(ctx: Context<'_>, #[description = "Specific command to show help about"] command: Option<String>) -> Result<(), Error> {
-    let config = poise::builtins::HelpConfiguration { ..Default::default() };
-    poise::builtins::help(ctx, command.as_deref(), config).await?;
     Ok(())
 }
